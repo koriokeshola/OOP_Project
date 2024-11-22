4d8bb32c5fd927518eb89f884f34526addc7d065
Renée Low <157589714+burntkayatoast@users.noreply.github.com>
2024-11-22 15:28:39 +0000

    Create achievements.py

---
+++ Concerto_Project/achievements.py
@@ -0,0 +1,9 @@
+class Achievements:
+    def __init__(self):
+        self.unlocked_achievements = []
+
+    def unlock(self, dialogue):
+        #checks if achievement is already in the list. If not, it adds it to the list.
+        if dialogue not in self.unlocked_achievements: 
+            self.unlocked_achievements.append(dialogue)
+            print(f"Achievement Unlocked: {dialogue}") #prints out the achievement the player unlocked.
