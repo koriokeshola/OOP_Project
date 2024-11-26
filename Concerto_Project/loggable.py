22f347de7060ae2b6bf37764cbb4e3f773fd2652
Renée Low <157589714+burntkayatoast@users.noreply.github.com>
2024-11-26 12:36:53 +0000

    added comments

--- Concerto_Project/loggable.py
+++ Concerto_Project/loggable.py
@@ -1,17 +1,20 @@
 class Loggable:
     def __init__(self):
-        self._logs = []
+        self._logs = [] #creates private list to store log messages
 
+    #getter for private logs list
+    #is read-only access to the logs outside the class
     @property
     def logs(self):
         return self._logs
 
     def log(self, message: str):
-        self._logs.append(message)
+        self._logs.append(message) #adds a new message to the logs list
 
     def save_logs_to_file(self, filename):
-        f = open(filename, 'w')
+        #saves current logs to a specified file
+        f = open(filename, 'w') #opens file in write mode
         for log in self._logs:
-            f.write(log + '\n')
-        f.close()
-        print(f"Logs saved to {filename}")
+            f.write(log + '\n') #writes each log entry, then adds a newline
+        f.close() #closes file after writing
+        print(f"Logs saved to {filename}") #prints out message to confirm the save
