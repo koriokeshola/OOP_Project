c67e069251408d4ca84945bc6f31b77a7193ac6d
ThatGuySob <101882926+ThatGuySob@users.noreply.github.com>
2024-11-28 10:40:56 +0000

    Add files via upload

---
+++ Concerto_Project/reviews.py
@@ -0,0 +1,46 @@
+from random import randint
+
+class Reviews: #review class defined
+    def __init__(self):
+        self.__list_of_reviews = [] #list for potential reviews
+
+    def add_bad_reviews(self, name): #method for adding negative reviews to the list
+        choice = randint(1, 5)
+        if choice == 1:
+            review = (f"{name}: They messed up my drink! Never coming back again. \U0001F644") #using unicode to print emojis 
+        elif choice == 2:
+            review = (f"{name}: How do you manage to mess up a drink so badly??? Don't recommend it!")
+        elif choice == 3:
+            review = (f"{name}: The customer service is horrible!")
+        elif choice == 4:
+            review = (f"{name}: I just wanted a sweet treat, and they messed it up completely...")
+        elif choice == 5:
+            review = (f"{name}: Last time you'll ever see me in that Café!!!")
+
+        self.__list_of_reviews.append(review) #appending the review with a randomiser from above
+
+    def add_good_reviews(self, name): #method for adding positive reviews to the list
+        choice = randint(1, 5)
+        if choice == 1:
+            review = (f"{name}: Got my order to the tea! Loved it and will come back for sure.")
+        elif choice == 2:
+            review = (f"{name}: Wonderful Customer Service, will be back in the future.")
+        elif choice == 3:
+            review = (f"{name}: Best place for perfect coffee, good service, and a wonderful atmosphere.\U0001f600") #using unicode to print emojis 
+        elif choice == 4:
+            review = (f"{name}: I'm so Addicted!! I'll only get my drinks from there now.")
+        elif choice == 5:
+            review = (f"{name}: I guess it's alright.")
+
+        self.__list_of_reviews.append(review) #appending the review with a randomiser from above
+
+    @property #getter decorator for accessing hidden list
+    def reviews(self):
+        return self.__list_of_reviews 
+
+    @reviews.setter #setter decorator for writing into the hidden list
+    def reviews(self, new_review):
+        if isinstance(new_review, str) and new_review: #opens the hidden list to append a new review
+            self.__list_of_reviews.append(new_review) #appends a new review to the list 
+
+    
