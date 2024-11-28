0d681a70b38dc81abc6c575fde56e8b855edab6c
BeSuper123 <49164980+BeSuper123@users.noreply.github.com>
2024-11-28 10:29:35 +0000

    Update makeDrink.py

--- Concerto_Project/makeDrink.py
+++ Concerto_Project/makeDrink.py
@@ -6,23 +6,25 @@ from characters import Character
 import random
 from time import sleep
 
+
 class MakeDrink:
     """ A class that manages the process of making drinks based on customer orders.
     There are coffee, tea and boba options, and user input is taken to make the drinks
     according to the customer's order."""
+
     def __init__(self):
-        self.drink = None #drink options (c,t,b)
-        self.drink_name = None #full length of drink's name (coffe, tea, boba)
-        self.made_drink = [] #stores drink details
-        self.character = Character() #customer interactions
-        self.size = None # s, m or l
-        self.milk = None # yes or no
-        self.sugar = None # 1, 2, or 3 spoons
-        self.temp = None # hot, cold
-        self.ice = None # yes, no, or extra
-        self.boba = None # normal or extra
-        self.log = Loggable() #instance for logging actions
-        self.error_log = Loggable() #instance logging errors
+        self.drink = None  # drink options (c,t,b)
+        self.drink_name = None  # full length of drink's name (coffe, tea, boba)
+        self.made_drink = []  # stores drink details
+        self.character = Character()  # customer interactions
+        self.size = None  # s, m or l
+        self.milk = None  # yes or no
+        self.sugar = None  # 1, 2, or 3 spoons
+        self.temp = None  # hot, cold
+        self.ice = None  # yes, no, or extra
+        self.boba = None  # normal or extra
+        self.log = Loggable()  # instance for logging actions
+        self.error_log = Loggable()  # instance logging errors
 
     def drink_options(self, name, day):
         """
@@ -34,16 +36,16 @@ class MakeDrink:
         day 5 = all three
         """
         drink_per_day = {
-            1: "c", # coffee only
-            2: "t", # tea only
-            3: random.choice(["c", "t"]), # coffee and tea
-            4: "b", # boba only
-            5: random.choice(["c", "t", "b"]) # all three
+            1: "c",  # coffee only
+            2: "t",  # tea only
+            3: random.choice(["c", "t"]),  # coffee and tea
+            4: "b",  # boba only
+            5: random.choice(["c", "t", "b"])  # all three
         }
 
         # finds out the day number
         day_chosen = drink_per_day[day]
-        
+
         if day_chosen == "c":
             self.character.coffee_option(name)
         elif day_chosen == "t":
@@ -51,23 +53,22 @@ class MakeDrink:
         elif day_chosen == "b":
             self.character.boba_option(name)
 
-
     # allows the user to choose the drink
     def drink_choice(self):
-        #allows user to choose make the drinks
+        # allows user to choose make the drinks
         self.drink = input("What drink do you want to make. \n"
                            "'c' for coffee\n"
                            "'t' for tea\n"
                            "'b' for boba\n"
                            "Enter the drink you want to make: ")
-        #checks if you've entered valid inputs
+        # checks if you've entered valid inputs
         if self.drink not in ["c", "t", "b"]:
             print("This is not a drink option, try again!\n")
             self.drink_choice()
         else:
-            #appends chosen drink type to the made_drink list
+            # appends chosen drink type to the made_drink list
             self.made_drink.append(self.drink)
-    
+
             if self.drink == "c":
                 self.drink_name = "coffee"
             elif self.drink == "t":
@@ -78,7 +79,7 @@ class MakeDrink:
         return self.drink
 
     def make_drink(self, name):
-        #making the drink options
+        # making the drink options
         self.size = input(f"Did {name} want small, medium, or large? [s, m, l]: ")  # s, m or l
         sleep(.5)
         while self.size not in ["s", "m", "l"]:
@@ -114,14 +115,13 @@ class MakeDrink:
                 print("Please pick y or n\n")
                 self.boba = input(f"Did {name} want extra boba? yes or no?: [y, n]: ")  # yes or no
 
-
         # appends options to the made_drink list
         self.made_drink.append(self.size)
         self.made_drink.append(self.milk)
         self.made_drink.append(self.sugar)
         self.made_drink.append(self.temp)
         self.made_drink.append(self.ice)
-        if (self.drink == "b"):
+        if self.drink == "b":
             self.made_drink.append(self.boba)
 
         return self.made_drink
